7651623a521930ea77625ce83171a90109e70890
AllenLyHjm <141938347+AllenLyHjm@users.noreply.github.com>
2023-08-12 01:16:09 +0800

    pull commit

--- src/main/java/com/itguigu/git/GitTest.java
+++ src/main/java/com/itguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master git.test");
         System.out.println("hor-fix git.test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
